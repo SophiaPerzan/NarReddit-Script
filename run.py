9d3249faa2e542c2d1f6479f2f501d83cff99b6b
Sophia <sophia@perzan.dev>
2023-06-19 19:46:12 -0400

    Basic video generation added

--- run.py
+++ run.py
@@ -2,13 +2,20 @@ from os import environ as env
 from dotenv import load_dotenv
 from scraper import Scraper
 from tts import TTS
+from videoGen import VideoGenerator
 
 load_dotenv()
 
-scraper = Scraper(env)
-post = scraper.getHotPosts()
-print("Scraped post: "+post[0])
+# scraper = Scraper(env)
+# post = scraper.getHotPosts()
+# print("Scraped post: "+post[0])
+#
+# tts = TTS(env)
+# audioFile = tts.createAudio(post[0], post[1])
+# print("Created audio file: " + audioFile)
 
-tts = TTS(env)
-title = tts.createAudio(post[0], post[1])
-print("Created audio file"+ title)
+videoGen = VideoGenerator(env)
+videoFile = videoGen.generateVideo(
+    'background-videos/MCParkour.mp4', 'speech.mp3', 'output/output.mp4')
+if (videoFile != False):
+    print("Created output video file at: " + videoFile)
--- videoGen.py
+++ videoGen.py
@@ -1,5 +1,37 @@
 import ffmpeg
+import os
+
 
 class VideoGenerator:
-    def __init__(self):
-        pass
+    def __init__(self, env):
+        self.env = env
+
+    def generateVideo(self, backgroundVideoPath, ttsAudioPath, outputVideoPath):
+
+        if not os.path.isfile(backgroundVideoPath):
+            print(f"Video file not found: {backgroundVideoPath}")
+            return False
+        if not os.path.isfile(ttsAudioPath):
+            print(f"Audio file not found: {ttsAudioPath}")
+            return False
+
+        video = ffmpeg.input(backgroundVideoPath)
+        audio = ffmpeg.input(ttsAudioPath)
+
+        # Get the duration of the audio file
+        probe = ffmpeg.probe(ttsAudioPath)
+        audio_duration = float(probe['streams'][0]['duration'])+2
+
+        # Trim the video to match the length of the audio
+        video = ffmpeg.filter_(video, 'trim', duration=audio_duration)
+
+        # Merge the video and audio together, and output to output_path
+        output = ffmpeg.output(video, audio, outputVideoPath)
+
+        # Overwrite the output file if it exists
+        output = ffmpeg.overwrite_output(output)
+
+        # Run the ffmpeg command
+        ffmpeg.run(output)
+
+        return outputVideoPath
