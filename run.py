3febd5e0fc2c2a92ef35bc444ea90426a9cfbd6b
Sophia <sophia@perzan.dev>
2023-06-20 18:39:56 -0400

    Added GPT-4 editing, voice change based on gender

--- run.py
+++ run.py
@@ -3,19 +3,28 @@ from dotenv import load_dotenv
 from scraper import Scraper
 from tts import TTS
 from videoGen import VideoGenerator
+import os
+from gpt import GPT
 
 load_dotenv()
 
-# scraper = Scraper(env)
-# post = scraper.getHotPosts()
-# print("Scraped post: "+post[0])
-#
-# tts = TTS(env)
-# audioFile = tts.createAudio(post[0], post[1])
-# print("Created audio file: " + audioFile)
+scraper = Scraper(env)
+post = scraper.getHotPosts()
+postTitle = post[0]
+postTitleAndText = post[1]
+print("Scraped post: "+postTitleAndText)
+
+gpt = GPT(env)
+gender = gpt.getGender(postTitleAndText)
+editedPost = gpt.expandAcronymsAndAbbreviations(postTitleAndText)
+
+tts = TTS(env)
+audioFile = tts.createAudio(editedPost, gender)
+print("Created audio file: " + audioFile)
 
 videoGen = VideoGenerator(env)
+outputPath = os.path.join('output', 'output.mp4')
 videoFile = videoGen.generateVideo(
-    'background-videos/MCParkour.mp4', 'speech.mp3', 'output/output.mp4')
+    'background-videos/MCParkour.mp4', audioFile, outputPath)
 if (videoFile != False):
     print("Created output video file at: " + videoFile)
--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import praw
 
+
 class Scraper:
     def __init__(self, env):
         self.env = env
@@ -7,18 +8,20 @@ class Scraper:
 
         # Read-only instance
         self.reddit = praw.Reddit(client_id=env['CLIENT_ID'],
-                            client_secret=env['CLIENT_SECRET'],
-                            user_agent=env['USER_AGENT'])
+                                  client_secret=env['CLIENT_SECRET'],
+                                  user_agent=env['USER_AGENT'])
         self.reddit.read_only = True
         self.subreddit = self.reddit.subreddit(env['SUBREDDIT'])
-    
+        self.minPostLength = int(env['MIN_POST_LENGTH'])
+        self.maxPostLength = int(env['MAX_POST_LENGTH'])
+
     def getHotPosts(self):
         for post in self.subreddit.hot():
-            if not post.stickied and post.is_self and len(post.selftext) < 1100 and len(self.hotPosts) < 2:
+            if not post.stickied and post.is_self and (self.minPostLength <= len(post.selftext) <= self.maxPostLength) and len(self.hotPosts) < 2:
                 self.hotPosts.append(post)
             if len(self.hotPosts) >= 2:
                 break
-            
+
         self.hotPosts = self.hotPosts[:1]
         post = self.hotPosts[0]
         return (post.title, post.title+"\n"+post.selftext)
